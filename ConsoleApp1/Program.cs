246af89cae65b1cb5b7a8cb9948ad7b6df20802c
Filipe do Amaral Padilha <filipe.padilha@maxima.local>
2024-09-04 19:11:15 -0300

    Test commit comment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace Primeiro
             Console.WriteLine("Test Visual Studio 2");
             //<type> <name> = <inicial value> opcional
             int n = 25;
-
+            //comment
         }
     }
 }
