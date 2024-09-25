5711bd9b271e6b93732515c0939639f9647d6021
Filipe do Amaral Padilha <filipe.padilha@maxima.local>
2024-09-25 16:18:41 -0300

    Decimal places test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 namespace Primeiro
 {
     class Program
@@ -9,19 +10,35 @@ namespace Primeiro
             //<type> <name> = <inicial value> opcional  
             int n = 25;
 
-           /* VARIAVEIS EM C#
-            Não pode conter:
-            1 - Dígito no começo da variável - ex: int 5filipe
-            2 - Não pode conter espaços em branco - ex: int "Filipe Padilha"
-            3 - Não usar acentuação - ex: int Âncora 
+            /* VARIAVEIS EM C#
+             Não pode conter:
+             1 - Dígito no começo da variável - ex: int 5filipe
+             2 - Não pode conter espaços em branco - ex: int "Filipe Padilha"
+             3 - Não usar acentuação - ex: int Âncora 
 
-            Explos de variáveis corretas:
-            1 - Pode conter _ - ex: int _Filipe
-            2 - int _5Filipe
-            3 - int FilipePadilha
-            4 - int filipePadilha
+             Explos de variáveis corretas:
+             1 - Pode conter _ - ex: int _Filipe
+             2 - int _5Filipe
+             3 - int FilipePadilha
+             4 - int filipePadilha
 
-            Uma boa prática é seguir o padrão CamelCase*/
+             Uma boa prática é seguir o padrão CamelCase*/
+
+            //Escrevendo variáveis em C# na tela
+            double x = 10.5243;
+            
+            //Se eu quiser pular uma linha é "Console.WriteLine"
+            Console.WriteLine(x);
+
+            //Se eu quiser imprimir tudo na mesma linha, sem pular uma linha é somente "Console.Write"
+            Console.Write(x);
+            Console.Write(" Mesma Linha");
+
+            //Se eu quiser alterar o número de casas decimais
+            Console.WriteLine("\n");
+            Console.WriteLine(x.ToString("F2")); //Aqui está saindo com vírgula por causa da linguagem do Visual Studio em pt-br
+
+            //Se quiser que saia com "." na quebra de casa decimal então eu preciso importar a bb using System.Globalization
         }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246af89cae65b1cb5b7a8cb9948ad7b6df20802c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61af660517cc457302755684e704bf5bf6300c6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
